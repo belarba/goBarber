75c35d5123e6d7f41fc89218d9d9f9d556e5456b
Daniel Belarmino <belarba@gmail.com>
2019-08-29 17:56:23 -0300

    canceling appointmend and sending email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '0af21bf223fc4a',
+    pass: 'ae787092957352',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
